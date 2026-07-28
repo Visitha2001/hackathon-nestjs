7eb6b9d42d8131a00834f8bc5e6d15ff951976e3
Visitha Nirmal Rajapaksha <visitha2001@gmail.com>
2026-07-28 14:38:14 +0530

    fix: prisma migration issue

--- src/lib/auth/auth.ts
+++ src/lib/auth/auth.ts
@@ -1,6 +1,6 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import { PrismaClient } from "../../../generated/prisma/client.js";
+import { PrismaClient } from "../../generated/prisma/client.js";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { Pool } from 'pg';
 import 'dotenv/config';
@@ -8,6 +8,9 @@ import 'dotenv/config';
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 const adapter = new PrismaPg(pool);
 const prisma = new PrismaClient({ adapter });
+// console.log("PRISMA KEYS:", Object.keys(prisma));
+// console.log("prisma.user:", !!prisma.user);
+// console.log("prisma.User:", !!(prisma as any).User);
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
