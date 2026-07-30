d72f56cd8ee2e57f26d9589dcb4cb943c23389d2
Visitha Nirmal Rajapaksha <visitha2001@gmail.com>
2026-07-30 14:08:10 +0530

    feat: implement global response interceptor with custom message decorator support

---
+++ src/common/interceptors/response.interceptor.ts
@@ -0,0 +1,40 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { RESPONSE_MESSAGE_KEY } from '../decorators/response-message.decorator';
+
+export interface Response<T> {
+  statusCode: number;
+  message: string;
+  data: T;
+}
+
+@Injectable()
+export class ResponseInterceptor<T> implements NestInterceptor<T, Response<T>> {
+  constructor(private reflector: Reflector) {}
+
+  intercept(context: ExecutionContext, next: CallHandler): Observable<Response<T>> {
+    return next.handle().pipe(
+      map((data) => {
+        const message = this.reflector.get<string>(
+          RESPONSE_MESSAGE_KEY,
+          context.getHandler(),
+        ) || 'Success';
+
+        const statusCode = context.switchToHttp().getResponse().statusCode;
+
+        return {
+          statusCode,
+          message,
+          data,
+        };
+      }),
+    );
+  }
+}
