e7ae02b4fb645e98be34aee01342934ed8a4b1f6
Visitha Nirmal Rajapaksha <visitha2001@gmail.com>
2026-08-02 10:53:13 +0530

    feat: initialize app bootstrap with global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory, Reflector } from '@nestjs/core';
+import { ValidationPipe, BadRequestException } from '@nestjs/common';
 import { AppModule } from './app.module';
 import 'dotenv/config';
 import { ResponseInterceptor } from './common/interceptors/response.interceptor';
@@ -9,6 +10,19 @@ async function bootstrap() {
   });
 
   app.useGlobalInterceptors(new ResponseInterceptor(app.get(Reflector)));
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      exceptionFactory: (errors) => {
+        const formattedErrors = errors.map((error) => ({
+          property: error.property,
+          message: Object.values(error.constraints || {}).join(', '),
+        }));
+        return new BadRequestException(formattedErrors);
+      },
+    }),
+  );
 
   await app.listen(process.env.PORT ?? 3000);
 }
